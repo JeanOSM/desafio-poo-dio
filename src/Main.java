d13e9d4df766e12a912ba896571bca0ad4965716
JeanOSM <jean.moraess1211@gmail.com>
2024-12-10 22:47:10 -0300

    Primeiros passos no desafio.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Analise de dados");
+        curso1.setDescricao("Desenvolvendo a analise ");
+        curso1.setCargaHoraria(88);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Java");
+        curso2.setDescricao("Desenvolvimento com Java ");
+        curso2.setCargaHoraria(90);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Usando a IDE InteliJ");
+        mentoria.setdescricao("Aprenda a utilizar a IDE de forma mais eficiente");
+        mentoria.setData(LocalDate.now());
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria );
+
     }
 }
